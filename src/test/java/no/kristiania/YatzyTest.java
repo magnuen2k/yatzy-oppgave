640ac6ea49c0335a962396f9e459e43c9a509959
magnuen2k <maen030@student.kristiania.no>
2020-08-26 14:48:35 +0200

    Added new tests

--- src/test/java/no/kristiania/YatzyTest.java
+++ src/test/java/no/kristiania/YatzyTest.java
@@ -10,4 +10,11 @@ public class YatzyTest {
     public void testScore() {
         assertEquals(4, new YatziGame().score("ONES", new int[] { 1, 1, 2, 3, 2 }));
     }
+
+    @Test
+    void shouldCalculateForOnes() {
+        assertEquals(0, new YatziGame().score("ONES", new int[] { 2, 3, 4, 5, 6 }));
+        assertEquals(3, new YatziGame().score("ONES", new int[] { 2, 1, 4, 1, 1 }));
+        assertEquals(5, new YatziGame().score("ONES", new int[] { 1, 1, 1, 1, 1 }));
+    }
 }
