8fb69cb286dc3f485966b33ee70a452d6730d106
magnuen2k <maen030@student.kristiania.no>
2020-08-26 15:52:49 +0200

    add more tests

--- src/main/java/no/kristiania/YatziGame.java
+++ src/main/java/no/kristiania/YatziGame.java
@@ -2,6 +2,30 @@ package no.kristiania;
 
 public class YatziGame {
     public int score(String category, int[] dice) {
-        return 4;
+        int score = 0;
+        if(category.equals("ONES")){
+            for (int die : dice) {
+                if(die == 1){
+                    score += 1;
+                }
+            }
+        }
+
+        if(category.equals("TWOS")){
+            for (int die : dice) {
+                if(die == 2){
+                    score += 2;
+                }
+            }
+        }
+
+        if(category.equals("THREES")) {
+            for (int die : dice) {
+                if(die == 3){
+                    score += 3;
+                }
+            }
+        }
+    return score;
     }
 }
--- src/test/java/no/kristiania/YatzyTest.java
+++ src/test/java/no/kristiania/YatzyTest.java
@@ -6,15 +6,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YatzyTest {
 
+    /*
     @Test
     public void testScore() {
         assertEquals(4, new YatziGame().score("ONES", new int[] { 1, 1, 2, 3, 2 }));
     }
+    */
 
     @Test
     void shouldCalculateForOnes() {
         assertEquals(0, new YatziGame().score("ONES", new int[] { 2, 3, 4, 5, 6 }));
         assertEquals(3, new YatziGame().score("ONES", new int[] { 2, 1, 4, 1, 1 }));
         assertEquals(5, new YatziGame().score("ONES", new int[] { 1, 1, 1, 1, 1 }));
+    }
+
+    @Test
+    void shouldCalculateForTwos() {
+        assertEquals(4, new YatziGame().score("TWOS", new int[] { 2, 3, 2, 1, 4 }));
+    }
+
+    @Test
+    void shouldCalculateForThrees() {
+        assertEquals(6, new YatziGame().score("THREES", new int[] { 2, 3, 2, 1, 3 }));
     }
 }
